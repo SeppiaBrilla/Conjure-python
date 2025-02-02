b2478b366cc49aac639e01a31e7674ed8343a3bd
seppiabrilla <alessio.pellegrino@studio.unibo.it>
2025-02-03 00:53:13 +0100

    bug fix

--- model.py
+++ model.py
@@ -99,23 +99,24 @@ class EssenceModel:
                 return False
         return True
 
-print(EssenceModel(model="""language Essence 1.3
-given t : int(1..) $ strength (size of subset of rows)
-given k : int(1..) $ rows
-given g : int(2..) $ number of values
-given b : int(1..) $ columns
-where k>=t, b>=g**t
-find CA: matrix indexed by [int(1..k), int(1..b)] of int(1..g)
-such that
-    forAll rows : sequence (size t) of int(1..k) .
-        (forAll i : int(2..t) . rows(i-1) < rows(i)) ->
-        forAll values : sequence (size t) of int(1..g) .
-            exists column : int(1..b) .
-                forAll i : int(1..t) .
-                    CA[rows(i), column] = values(i)
+if __name__ == "__main__":
+    print(EssenceModel(model="""language Essence 1.3
+    given t : int(1..) $ strength (size of subset of rows)
+    given k : int(1..) $ rows
+    given g : int(2..) $ number of values
+    given b : int(1..) $ columns
+    where k>=t, b>=g**t
+    find CA: matrix indexed by [int(1..k), int(1..b)] of int(1..g)
+    such that
+        forAll rows : sequence (size t) of int(1..k) .
+            (forAll i : int(2..t) . rows(i-1) < rows(i)) ->
+            forAll values : sequence (size t) of int(1..g) .
+                exists column : int(1..b) .
+                    forAll i : int(1..t) .
+                        CA[rows(i), column] = values(i)
 
-such that forAll i : int(2..k) . CA[i-1,..] <=lex CA[i,..]
-such that forAll i : int(2..b) . CA[..,i-1] <=lex CA[..,i]
-""").solve({'t' : 3, 'g' : 2, 'k' : 4, 'b' : 8}))
+    such that forAll i : int(2..k) . CA[i-1,..] <=lex CA[i,..]
+    such that forAll i : int(2..b) . CA[..,i-1] <=lex CA[..,i]
+    """).solve({'t' : 3, 'g' : 2, 'k' : 4, 'b' : 8}))
 #sat instance {'t' : 3, 'g' : 2, 'k' : 4, 'b' : 8}
 #unsat instance {'t' : 3, 'g' : 2, 'k' : 5, 'b' : 8}
