d6039a8482008c05c6bc37eca8660613869fcd54
seppiabrilla <alessio.pellegrino@studio.unibo.it>
2025-02-04 17:54:17 +0100

    updated example

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 from conjure_python import EssenceModel
+import json
 
 model = EssenceModel(model="""language Essence 1.3
     given t : int(1..) $ strength (size of subset of rows)
@@ -24,3 +25,52 @@ model.set_random_seed(0)
 model.set_time_limit(1000)
 print(model.solve({'t' : 3, 'g' : 2, 'k' : 4, 'b' : 8}))
 
+model.clear()
+model.append("""language Essence 1.3
+$ prob001.essence: Car Sequencing
+$ Problem details available at http://www.csplib.org/Problems/prob001/
+
+given n_cars, n_classes, n_options : int(1..)
+
+letting Slots  be domain int(1..n_cars),
+        Class  be domain int(1..n_classes),
+        Option be domain int(1..n_options),
+
+given quantity      : function (total) Class  --> int(1..),
+      maxcars       : function (total) Option --> int(1..),
+      blksize_delta : function (total) Option --> int(1..),
+      usage         : relation (minSize 1) of ( Class * Option )
+
+$ There must be at least as many cars as there are n_classes as quantity is indexed from 1..
+where n_cars >= n_classes
+
+$ The sum of the cars in the quantity function should equal n_cars 
+where ( sum quant : Class . quantity(quant) ) = n_cars
+
+$ Blksize must be greater than maxcars for all options
+$ where forAll option: Option . maxcars(option) < blksize(option)
+
+$ Make sure that all options are used at least once
+where  forAll option: Option .  |toSet(usage(_,option))| >= 1
+
+$ Make sure that all classes have at least one option
+where  forAll class: Class .  |toSet(usage(class,_))| >= 1
+
+find car : function (total) Slots --> Class
+
+such that
+    forAll c : Class . |preImage(car,c)| = quantity(c),
+    forAll opt : Option .
+        forAll s : int(1..n_cars+1-(maxcars(opt)+blksize_delta(opt))) .
+            (sum i : int(s..s+(maxcars(opt)+blksize_delta(opt))-1) . toInt(usage(car(i),opt))) <= maxcars(opt)""")
+instance = {
+    "blksize_delta": {"1": 1, "2": 1, "3": 2, "4": 3, "5": 4}, 
+    "maxcars": {"1": 1, "2": 2, "3": 1, "4": 2, "5": 1}, "n_cars": 100, "n_classes": 18, "n_options": 5, 
+    "quantity": {"1": 5, "10": 6, "11": 12, "12": 1, "13": 1, "14": 5, "15": 9, "16": 5, "17": 12, "18": 1, 
+                 "2": 3, "3": 7, "4": 1, "5": 10, "6": 2, "7": 11, "8": 5, "9": 4},
+    "usage":[[1, 1], [1, 2], [1, 5], [2, 1], [2, 2], [2, 4], [3, 1], [3, 2], [3, 3], [4, 2], [4, 3], [4, 4], [5, 1], [5, 2],
+             [6, 1], [6, 5], [7, 1], [7, 4], [8, 1], [8, 3], [9, 2], [9, 5], [10, 2], [10, 4], [11, 2], [11, 3], [12, 3],
+             [12, 5], [13, 3], [13, 4], [14, 1], [15, 2], [16, 5], [17, 4], [18, 3]]
+}
+
+print(model.solve(instance))
