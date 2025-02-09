ec3280ccb38ad4e26c571b4610683bf4dca4e799
seppiabrilla <alessio.pellegrino@studio.unibo.it>
2025-02-09 20:21:23 +0100

    added generic methods to the father class

--- conjure_python/essenceTypes.py
+++ conjure_python/essenceTypes.py
@@ -13,6 +13,15 @@ class EssenceType:
     def __init__(self, values:dict, essece_types:str) -> None:
         pass
 
+    def __len__(self) -> int:
+        raise NotImplementedError("method __len__ not implemented. It must be implemented by the children class")
+
+    def __hash__(self) -> int:
+        raise NotImplementedError("method __hash__ not implemented. It must be implemented by the children class")
+
+    def __str__(self) -> str:
+        raise NotImplementedError("method __len__ not implemented. It must be implemented by the children class")
+
 class EssenceMatrix(EssenceType):
     def __init__(self, values:dict, essece_types:str) -> None:
         super().__init__(values, essece_types)
