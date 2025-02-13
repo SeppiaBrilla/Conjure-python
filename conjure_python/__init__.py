eb413712a74c066e391c92333395b84584a69443
seppiabrilla <alessio.pellegrino@studio.unibo.it>
2025-02-13 23:30:25 +0100

    updated exposed elements

--- conjure_python/__init__.py
+++ conjure_python/__init__.py
@@ -1,4 +1,4 @@
 from .conjure import is_conjure_available
-from .essenceTypes import EssenceType, EssenceMatrix, EssenceFunction
+from .essenceTypes import EssenceType, EssenceMatrix, EssenceFunction, EssenceRecord, EssenceRelation
 from .model import EssenceModel
-from .solution import EssenceSolution
+from .solution import EssenceSolution, SAT, UNSAT
