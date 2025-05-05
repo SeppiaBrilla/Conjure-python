9a7bdad0dcfb524787ddf11cbb6c9e68b24de5f3
seppiabrilla <alessio.pellegrino@studio.unibo.it>
2025-05-05 20:48:19 +0200

    removed main call

--- conjure_python/conjure.py
+++ conjure_python/conjure.py
@@ -87,29 +87,3 @@ class Conjure:
 
 def is_conjure_available() -> bool:
     return Conjure.available()
-
-if __name__ == "__main__":
-    model = '''
-language Essence 1.3
-given t : int(1..) $ strength (size of subset of rows)
-given k : int(1..) $ rows
-given g : int(2..) $ number of values
-given b : int(1..) $ columns
-where k>=t, b>=g**t
-find CA: matrix indexed by [int(1..k), int(1..b)] of int(1..g)
-such that
-    forAll rows : sequence (size t) of int(1..k) .
-        (forAll i : int(2..t) . rows(i-1) < rows(i)) ->
-        forAll values : sequence (size t) of int(1..g) .
-            exists column : int(1..b) .
-                forAll i : int(1..t) .
-                    CA[rows(i), column] = values(i)
-such that forAll i : int(2..k) . CA[i-1,..] <=lex CA[i,..]
-such that forAll i : int(2..b) . CA[..,i-1] <=lex CA[..,i]
-'''
-    instance = json.dumps({'t' : 3, 'g' : 2, 'k' : 5, 'b' : 8})
-
-    conjure = Conjure()
-    print(f'conjure available: {Conjure.available()}')
-    print(conjure.solve(model, instance))
-    print(conjure.pretty_print(model,'astjson'))
